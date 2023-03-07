a0d5d47cd2487db1447db39ffb992bd7a9e135b1
Sumit Saurabh <sumitsaurabh112@gmail.com>
2023-03-07 20:23:45 +0530

    product slider

---
+++ script.js
@@ -0,0 +1,60 @@
+import { imageSlider } from "./utilities/data.js"
+
+
+
+/*******************image Slider********************* */
+let imageSliderListEl = document.querySelector(".imageSliderList")
+let imageSliderListHTML = ''
+console.log(imageSlider)
+
+
+imageSlider.forEach(el => {
+    imageSliderListHTML += `
+    <div class="imageSliderItem">
+        <a href="${el.link}">
+            <img src="${el.img}" />
+        </a>
+    </div>
+    `
+})
+imageSliderListEl.innerHTML = imageSliderListHTML;
+
+let preve_imageBtnEl = document.getElementById("preve_imageBtn")
+let next_imageBtn = document.getElementById("next_imageBtn")
+let start = 0;
+let end = -400;
+
+preve_imageBtnEl.addEventListener("click", handlePreveImage)
+next_imageBtn.addEventListener("click", handleNextImage)
+
+function handlePreveImage() {
+    let imageallList = document.querySelectorAll(".imageSliderItem")
+    console.log(imageallList)
+    if (start < 0)
+        start += 100
+    imageallList.forEach(el => {
+        el.style.transform = `translateX(${start}%)`
+    })
+
+}
+function handleNextImage() {
+    let imageallList = document.querySelectorAll(".imageSliderItem")
+    console.log(imageallList)
+    if (start > end)
+        start -= 100
+    imageallList.forEach(el => {
+        el.style.transform = `translateX(${start}%)`
+    })
+}
+
+function renderImageSlider() {
+    if (start > end) {
+        handleNextImage()
+    }
+    else {
+        start = 100
+    }
+}
+
+setInterval(renderImageSlider, 1000)
+
